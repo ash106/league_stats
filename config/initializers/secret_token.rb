f473836514e28c9df8cee63d8a04f8010a8b32f3
Alex Howington <alex.howington@gmail.com>
2013-12-03 19:08:18 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeagueStats::Application.config.secret_key_base = '9fe1e36eaf955b36dbe075d6bf7080c0a22f9997f8c1701c7f75b7ac59226cdfa22d4d17b32b16ca68aa191041611acc123b6dba434d2605b038a21d25be4f8f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeagueStats::Application.config.session_store :cookie_store, key: '_league_stats_session'
